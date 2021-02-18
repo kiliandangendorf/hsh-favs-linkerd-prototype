a6b036799d763d5298385fb5b0195245a74d80f6
Christopher Rust <christopher.rust@stud.hs-hannover.de>
2021-02-18 19:21:23 +0100

    python requi hinzugefügt

--- src/goodbye-world-service/index.js
+++ src/goodbye-world-service/index.js
@@ -3,8 +3,16 @@ var express = require('express')
 var app = express()
 //Define request response in root URL (/) and response with text Hello World!
 app.get('/', function (req, res) {
+  console.log('get')
   res.send('Goodbye World!')
 })
+
+// POST method route
+app.post('/', function (req, res) {
+  console.log('post')
+  res.send('POST request to the homepage');
+});
+
 //Launch listening server on port 8080 and consoles the log.
 app.listen(8080, function () {
   console.log('app listening on port 8080!')
