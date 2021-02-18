37a8871f4bdf33c2235042ebfcbcd5aff777b36e
Christopher Rust <christopher.rust@stud.hs-hannover.de>
2021-02-18 15:07:10 +0100

    erste stand

---
+++ src/docker-build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+docker build --tag=microservice-linkerd-apache apache
+docker build --tag=microservice-linkerd-goodbyewolrd:1 goodbye-world-service
+docker build --tag=microservice-linkerd-helloworld:1 hello-world-service
+docker build --tag=microservice-linkerd-nameapi:1 name-service
