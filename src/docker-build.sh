e17df08cd3dae4e1be2a3bf0e759d01cc3d825b3
Christopher Rust <christopher.rust@stud.hs-hannover.de>
2021-02-18 20:46:01 +0100

    ports gerade gebogen

--- src/docker-build.sh
+++ src/docker-build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 docker build --tag=microservice-linkerd-apache apache
-docker build --tag=microservice-linkerd-goodbyewolrd:1 goodbye-world-service
+docker build --tag=microservice-linkerd-goodbyeworld:1 goodbye-world-service
 docker build --tag=microservice-linkerd-helloworld:1 hello-world-service
 docker build --tag=microservice-linkerd-nameapi:1 name-service
