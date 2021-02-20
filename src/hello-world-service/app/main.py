be64ae3ceaf7b5ec146503f6f396062e52227802
Kilian Dangendorf <kilian.dangendorf@stud.hs-hannover.de>
2021-02-20 14:06:46 +0100

    helloworld now communicates with nameapi

--- src/hello-world-service/app/main.py
+++ src/hello-world-service/app/main.py
@@ -3,10 +3,22 @@ import requests
 
 from fastapi import FastAPI
 
-app = FastAPI()
+import logging
 
+#app = FastAPI()
+app = FastAPI(openapi_prefix="/helloworld")
 
-@app.get("/{item_id}")
+@app.get("/helloworld/")
+def read_root():
+    logging.info('calling "/helloworld/"-root')
+    return {"Hello": "World"}
+
+@app.get("/helloworld/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
-    response = requests.get("/nameapi/items/{}".format(item_id))
-    return {"Hello World {}".format(response)}
+    logging.info('calling "/helloworld/"-items')
+    host='nameapi.default.svc.cluster.local:80'
+    get_test_url= f"http://{host}/nameapi/items/{item_id}/"
+    response = requests.get(get_test_url).json()
+    name=response["name"]
+    return {"text": "Hello World {}".format(name)}
+
