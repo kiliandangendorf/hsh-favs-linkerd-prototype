0635425034a6d4d51b91231ae3303ba33b508d0d
Manuel Ottlik <28919003+manuelottlik@users.noreply.github.com>
2021-02-22 10:41:15 +0100

    strip prefix

--- src/hello-world-service/app/main.py
+++ src/hello-world-service/app/main.py
@@ -5,19 +5,18 @@ from fastapi import FastAPI
 
 import logging
 
-#app = FastAPI()
-app = FastAPI(openapi_prefix="/helloworld")
+app = FastAPI()
 
-@app.get("/helloworld/")
+@app.get("/")
 def read_root():
     logging.info('calling "/helloworld/"-root')
     return {"Hello": "World"}
 
-@app.get("/helloworld/items/{item_id}")
+@app.get("/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     logging.info('calling "/helloworld/"-items')
     host=os.getenv('NAME_SERVICE_HOST', 'nameapi.default.svc.cluster.local:80')
-    get_test_url= f"http://{host}/nameapi/items/{item_id}/"
+    get_test_url= f"http://{host}/items/{item_id}/"
     response = requests.get(get_test_url).json()
     name=response["name"]
     return {"text": "Hello World {}".format(name)}
